d8122bebc1edf564752204eccd4eca72cd1d3dd8
bitwize01 <bitwize01@gmail.com>
2017-07-19 13:41:48 -0400

    Initial Commit
    First commit of the NUnit demo solution, with 5 test cases.

---
+++ NUnitExample/AccountTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitExample
+{
+    [TestFixture]
+    public class AccountTest
+    {
+        Account source;
+        Account destination;
+
+        [SetUp]
+        public void Init()
+        {
+            source = new Account();
+            source.Deposit(200m);
+
+            destination = new Account();
+            destination.Deposit(150m);
+        }
+
+        [Test]
+        public void TransferFunds()
+        {
+            source.TransferFunds(destination, 100m);
+
+            Assert.AreEqual(250m, destination.Balance);
+            Assert.AreEqual(100m, source.Balance);
+        }
+
+        [Test]
+        public void TransferWithInsufficientFunds()
+        {
+            Assert.That(() => source.TransferFunds(destination, 300m), Throws.TypeOf<InsufficientFundsException>());
+        }
+
+        [Test]
+        [Ignore("Decide how to implement transaction management")]
+        public void TransferWithInsufficientFundsAtomicity()
+        {
+            try
+            {
+                source.TransferFunds(destination, 300m);
+            }
+            catch (InsufficientFundsException expected)
+            {
+                System.Console.WriteLine(expected.Message);
+            }
+
+            Assert.AreEqual(200m, source.Balance);
+            Assert.AreEqual(150m, destination.Balance);
+        }
+    }
+}
---
+++ NUnitExample/BankSample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NUnitExample
+{
+    public partial class BankSample : Form
+    {
+        public BankSample()
+        {
+            InitializeComponent();
+        }
+    }
+}
